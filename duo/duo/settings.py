9e438dc0610ebcebfd8c91f0ed540ca2bb7e8401
Igor <iskoric95@gmail.com>
2025-04-02 12:22:32 +0200

    allowedhosts

--- duo/duo/settings.py
+++ duo/duo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4pw$gkrj_%a6gy28lxm&-tsul$3&bwd1dt9w=4b$oe-6+&el_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['duopluspro.rs', '127.0.0.1']
 
 
 # Application definition
